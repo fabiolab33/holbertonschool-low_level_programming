0700f83f4f3b0ff11aad0db02b6cbcff4af8e546
fabiola <fabiola@penguin>
2025-10-15 10:05:44 -0400

    Text print

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-/*
- * main - Entry point of the script
+
+/**
+ * main - Prints a message using puts
  *
- * Return: Always Zero
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
