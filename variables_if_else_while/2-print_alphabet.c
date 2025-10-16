021887385d9626955e89298340e74dea570800d8
fabiola <fabiola@penguin>
2025-10-15 20:07:58 -0400

    Alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: Prints the lowercase alphabet followed by new line
+ *
+ * Return: Always 0 (Succes)
+ */
+int main(void)
+{
+char letter;
+
+for (letter = 'a'; letter <= 'z'; letter++)
+putchar(letter);
+
+putchar('\n');
+
+return (0);
+}
