580ebaa85d57240ae9c073b8334eaf9dad0502c2
fabiola <fabiola@penguin>
2025-10-21 19:51:15 -0400

    ###

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_square - print a square using #
+ * @size: Size of the square
+ *
+ * Description: If the size is 0 or less, print only a new line
+ */
+void print_square(int size)
+{
+int i, j;
+
+if (size <= 0)
+{
+_putchar('\n');
+return;
+}
+
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size; j++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
