5cca0ef9fedf1b79ee68f59dff69a1e04a2c6e65
fabiola <fabiola@penguin>
2025-10-22 10:38:40 -0400

    Updates the value

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates the value of the integer pointed to by n to 98
+ * @n: Pointer to an int to be updated.
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
