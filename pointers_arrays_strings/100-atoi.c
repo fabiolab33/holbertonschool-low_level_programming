299a7acb4ffd6a1a5e05ed84584f76d0e7e20f9c
fabiola <fabiola@penguin>
2025-10-25 19:17:22 -0400

    Convert a string

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -19,19 +19,25 @@ if (s[i] == '-')
 sing *= -1;
 else if (s[i] == '+')
 ;
-
-
 else if (s[i] >= '0' && s[i] <= '9')
 {
 started = 1;
-result = result * 10 + (s[i] - '0');
-}
+digit = s[i] - '0';
 
+if (result > (2147483647 - digit) / 10)
+{
+if (sign == 1)
+return (2147483647);
+else
+return (-2147483648);
+}
+result = result * 10 + digit;
+}
 else if (started)
 break;
 
 i++;
 }
 
-return (result * sing);
+return (result *sing);
 }
