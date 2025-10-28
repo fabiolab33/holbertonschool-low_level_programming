9111b015240276ae0d29cc1f85952dd6d6428f21
fabiola <fabiola@penguin>
2025-10-27 20:53:09 -0400

    Gets the length of a prefix substring

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -14,9 +14,9 @@ unsigned int count = 0;
 int i, j;
 int found;
 
- for (i = 0; s[i] != '\0'; i++)
- {
- found =0;
+for (i = 0; s[i] != '\0'; i++)
+{
+found =0;
 for (j = 0; accept[j] != '\0'; j++)
 {
 if (s[i] == accept[j])
@@ -26,10 +26,8 @@ break;
 }
 }
 if (found == 0)
-  return (count);
+return (count);
 count++;
 }
 return (count);
 }
-
- 
