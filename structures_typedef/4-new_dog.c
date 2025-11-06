90d0b0cfd6b97f515d12e80cbbb9bfaf707ef4eb
fabiola <fabiola@penguin>
2025-11-06 14:13:54 -0400

    CReates a new dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -14,34 +14,40 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 dog_t *d;
-
+int i, len;
+ 
 d = malloc(sizeof(dog_t));
 if (d == NULL)
 return (NULL);
 
 if (name != NULL)
 {
-d->name = malloc(strlen(name) + 1);
+for (len = 0; name[len]; len++)
+;
+d->name = malloc(len + 1);
 if (d->name == NULL)
 {
 free(d);
 return (NULL);
 }
-strcpy(d->name, name);
+for (i = 0; i <= len; i++)
+d->name[i] = name[i];
 }
 else
 d->name = NULL;
 
 if (owner != NULL)
 {
-d->owner = malloc(strlen(owner) + 1);
-if (d->owner == NULL)
+for (len = 0; owner[len]; len++)
+;
+d->owner = malloc(len + 1);
 {
 free(d->name);
 free(d);
 return (NULL);
 }
-strcpy(d->owner, owner);
+for (i = 0; i <= len; i++)
+d->owner[i] = owner[i];
 }
 else
 d->owner = NULL;
