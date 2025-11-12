2d80b21e9c17420cf5f6599e80b5cc4782d0bef6
fabiola <fabiola@penguin>
2025-11-12 17:40:41 -0400

    Performs simple operations

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,41 @@
+#include "3-calc.h"
+
+/**
+ * op_add - returns the sum of a and b
+ */
+int op_add(int a, int b)
+{
+return (a + b);
+}
+
+/**
+ * op_add - returns the difference of a and b
+ */
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+
+/**
+ * op_add - returns the product of a and b
+ */
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+
+/**
+ * op_add - returns the division of a by b
+ */
+int op_div(int a, int b)
+{
+return (a / b);
+}
+
+/**
+ * op_add - returns the remainder of the division of a and b
+ */
+int op_mod(int a, int b)
+{
+return (a % b);
+}
