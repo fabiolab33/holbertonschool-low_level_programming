ec24b6c0405708bfc44118813319c3927bb812c2
fabiola <fabiola@penguin>
2025-11-15 15:10:23 -0400

    Prints anything

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -18,13 +18,13 @@ while (format && format[i])
 switch (format[i])
 {
 case 'c':
-printf("%s%c", separator, va_arg(args, int));
+printf("%s%c", sep, va_arg(args, int));
 break;
 case 'i':
-printf("%s%d", separator, va_arg(args, int));
+printf("%s%d", sep, va_arg(args, int));
 break;
 case 'f':
-printf("%s%f", separator, va_arg(args, double));
+printf("%s%f", sep, va_arg(args, double));
 break;
 case 's':
 str = va_arg(args, char *);
