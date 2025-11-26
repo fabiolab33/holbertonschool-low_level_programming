fd8cf62d68008e94a915e493fd19f7a13165b440
fabiola <fabiola@penguin>
2025-11-26 18:34:28 -0400

    Adds a new node

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -20,11 +20,11 @@ new_node = malloc(sizeof(list_t));
 if (new_node == NULL)
 return (NULL);
 
- new_node->str = strdup(str);
+new_node->str = strdup(str);
 if (new_node->str == NULL)
 {
 free(new_node);
- return (NULL);
+return (NULL);
 }
 
 new_node->len = 0;
